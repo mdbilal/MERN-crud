a03585ba92d27609a46fd9c6823ad5609aea851d
MD Bilal <mdbilalofficial@gmail.com>
2025-09-30 03:37:53 +0500

    mongo db connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const db_uri = 'mongodb+srv://mdbilalofficial_db_user:nodeapipassword@cluster0.merjpy6.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = 3000;
 
 app.listen(PORT, () => {
@@ -9,3 +11,7 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('hello from api')
 })
+
+mongoose.connect(db_uri)
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log(err));

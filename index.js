b658aba04be9f2c680fdbf70aa2529c58b234de7
MD Bilal <mdbilalofficial@gmail.com>
2025-09-30 02:57:47 +0500

    installed express & setup demo api

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('hello from console');
+}),
+
+app.get('/', (req, res) => {
+    res.send('hello from api')
+})

48744af88ed7c10253306cf9bbe7b893330a94a0
MD Bilal <mdbilalofficial@gmail.com>
2025-10-01 01:33:12 +0500

    added entity model

---
+++ models/product.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    },
+},
+{timestamps: true}
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
